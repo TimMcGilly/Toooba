32437a72e308123c4327a62ce98357dd702f7a77
Karlis Susters <ks980@cam.ac.uk>
2023-12-13 14:16:00 +0200

    add path to prefetchers

--- builds/Resources/Include_Common.mk
+++ builds/Resources/Include_Common.mk
@@ -33,6 +33,21 @@ help:
 all: compile  simulator
 
 # ================================================================
+# Path to RISCY-OOO sources
+
+RISCY_HOME ?= ../../src_Core/RISCY_OOO
+# RISCY_HOME ?= $(HOME)/Projects/RISCV/MIT-riscy/riscy-OOO
+
+RISCY_DIRS = $(RISCY_HOME)/procs/RV64G_OOO:$(RISCY_HOME)/procs/lib:$(RISCY_HOME)/coherence/src:$(RISCY_HOME)/coherence/src/prefetcher:$(RISCY_HOME)/fpgautils/lib
+
+CONNECTAL_DIRS = $(RISCY_HOME)/connectal/bsv:$(RISCY_HOME)/connectal/tests/spi:$(RISCY_HOME)/connectal/lib/bsv
+
+CHERI_DIRS = $(RISCY_HOME)/../../libs/cheri-cap-lib
+
+# ALL_RISCY_DIRS = $(RISCY_DIRS)
+ALL_RISCY_DIRS = $(EXTRA_DIRS):$(RISCY_DIRS):$(CONNECTAL_DIRS):$(CHERI_DIRS)
+
+# ================================================================
 # Search path for bsc for .bsv files
 
 TESTBENCH_DIRS = $(REPO)/src_Testbench/Top:$(REPO)/src_Testbench/SoC
